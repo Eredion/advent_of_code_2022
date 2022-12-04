db8e8698606ffdd4b49ba8cbccd4fed154d18e6a
Eredion <61424404+Eredion@users.noreply.github.com>
2022-12-04 20:20:04 +0100

    day4 finished

---
+++ day4/pairs2.py
@@ -0,0 +1,22 @@
+from input import input
+
+input_test = [
+((2,4),(6,8)),
+((2,3),(4,5)),
+((5,7),(7,9)),
+((2,8),(3,7)),
+((6,6),(4,6)),
+((2,6),(4,8)),
+]
+
+count = 0
+for line in input:
+    a, b = list(range(line[0][0], line[0][1] + 1)), list(range(line[1][0], line[1][1] + 1))
+    intersection = set(a).intersection(b)
+    if len(intersection):
+        count += 1
+
+print(count)
+
+
+
